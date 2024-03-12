78c003bf66319f4a874849b05e3e4d9f3c784770
sairamya-18 <sairamyatanneru@gmail.com>
2024-03-12 13:37:48 +0530

    Starting Now..All the Best

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+      <main>
+        <p>Start editing the App.js file to customize your application lol!</p>
+        <p> you could also try!!
+          
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
